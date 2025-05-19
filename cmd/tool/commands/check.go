3e94dc650ed2fc7dda5e0bfc95b02e6e8b74b243
l-qing <qingliu@alauda.io>
2025-05-19 13:31:50 +0800

    chore: Bump golang.org/x/net to v0.40.0 (#41)

--- cmd/tool/commands/check.go
+++ cmd/tool/commands/check.go
@@ -7,7 +7,7 @@ import (
 	"sort"
 
 	"github.com/Masterminds/semver"
-	"github.com/cli/go-gh"
+	gh "github.com/cli/go-gh/v2/pkg/api"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/cli"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
@@ -79,7 +79,7 @@ func checkComponentNewerVersions(component component, bugfix bool) (semver.Colle
 }
 
 func fetchVersions(github string) (semver.Collection, error) {
-	client, err := gh.RESTClient(nil)
+	client, err := gh.DefaultRESTClient()
 	if err != nil {
 		return nil, err
 	}
